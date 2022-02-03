4d253a80db7446c74481e0e8e0c24bbc4f0ad637
Arunan <Arunan2212@gmail.com>
2022-02-03 19:12:50 +0100

    Add Army

---
+++ src/main/java/Army.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
+
+public class Army {
+    private final String name;
+    private final ArrayList<Unit> units;
+
+    private Random rand = new Random();
+
+    public Army(String name) {
+        this.name = name;
+        this.units = new ArrayList<>();
+    }
+    public Army(String name, ArrayList<Unit> units) {
+        this.name = name;
+        this.units = units;
+    }
+
+
+    public void add (Unit unit) {
+        units.add(unit);
+    }
+
+    public void addAll (ArrayList<Unit> units) {
+        for (Unit unit: units) {
+            this.units.add(unit);
+        }
+    }
+
+    public void remove(Unit unit) {
+        units.removeIf(target -> target.equals(unit));
+    }
+
+    public boolean hasUnits() {
+        return units.isEmpty();
+    }
+
+    public Unit getRandom() {
+        return units.get(rand.nextInt(units.size()));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (Unit unit: this.units) {
+            sb.append(unit.toString());
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Army)) return false;
+        Army army = (Army) o;
+        return name.equals(army.name) && Objects.equals(units, army.units);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, units);
+    }
+}
