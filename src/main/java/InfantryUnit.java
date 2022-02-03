7a0796bf472ea265129c18d98488786f8081f28b
Arunan <Arunan2212@gmail.com>
2022-02-03 15:51:11 +0100

    Add InfantryUnit

--- src/main/java/InfantryUnit.java
+++ src/main/java/InfantryUnit.java
@@ -1,3 +1,18 @@
 public class InfantryUnit extends Unit {
+    private final int attackBonus = 2;
+    private final int resistBonus = 1;
 
+    public InfantryUnit(String type, int health) {
+        super(type, health, 15, 10);
+    }
+
+    @Override
+    public int getAttackBonus() {
+        return attackBonus;
+    }
+
+    @Override
+    public int getResistBonus() {
+        return resistBonus;
+    }
 }
