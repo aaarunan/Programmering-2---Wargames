28f7a1bd40d100d0f435f348203884e21b0a863b
Arunan <Arunan2212@gmail.com>
2022-02-03 15:05:57 +0100

    Add Unit superclass

---
+++ src/main/java/Unit.java
@@ -0,0 +1,62 @@
+
+
+public class Unit {
+
+    /**
+     *A Unit is a superclass for a troop.
+     */
+
+    private final String type;
+    private int healthPoints;
+    private int attackPoints;
+    private int armorPoints;
+
+    public Unit(String type, int healthPoints, int attackPoints, int armorPoints) {
+        this.type = type;
+        this.healthPoints = healthPoints;
+        this.attackPoints = attackPoints;
+        this.armorPoints = armorPoints;
+    }
+
+    public void attack(Unit opponent) {
+        int newHealthPoints = opponent.getHealthPoints()-attackPoints+this.getAttackBonus() + opponent.attackPoints +opponent.getResistBonus();
+        opponent.setHealthPoints(newHealthPoints);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public int getArmorPoints() {
+        return armorPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getAttackBonus() {
+
+    }
+
+    public int getResistBonus() {
+
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Type" + type +
+                " healthPoints=" + healthPoints +
+                " attackPoints=" + attackPoints +
+                " armorPoints=" + armorPoints;
+    }
+}
