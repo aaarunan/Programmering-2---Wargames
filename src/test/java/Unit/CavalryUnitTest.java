aea259009db693c2ed4126e01d340d02b115cb19
Arunan <Arunan2212@gmail.com>
2022-02-05 15:00:49 +0100

    Add CavalryUnitTest

--- src/test/java/Unit/CavalryUnitTest.java
+++ src/test/java/Unit/CavalryUnitTest.java
@@ -5,55 +5,46 @@ import junit.framework.TestCase;
 
 public class CavalryUnitTest extends TestCase {
 
+    CavalryUnit attacker = new CavalryUnit("Test1", 20);
+    CavalryUnit defender = new CavalryUnit("Test2", 15);
+
     public void testAttack() {
-        CavalryUnit test1 = new CavalryUnit("Test1", 20, 10, 5);
-        CavalryUnit test2 = new CavalryUnit("Test2", 15);
 
-        test1.attack(test2);
-        assertEquals(test1.getHealthPoints(), 20);
-        assertEquals(test2.getHealthPoints(), 12);
+        attacker.attack(defender);
+        assertEquals(attacker.getHealthPoints(), 20);
+        assertEquals(defender.getHealthPoints(), 12);
 
-        test1.attack(test2);
-        assertEquals(test1.getHealthPoints(), 20);
-        assertEquals(test2.getHealthPoints(), 12);
+        attacker.attack(defender);
+        assertEquals(attacker.getHealthPoints(), 20);
+        assertEquals(defender.getHealthPoints(), 12);
 
-        test1.attack(test2);
-        assertEquals(test2.getHealthPoints(), 12);
+        attacker.attack(defender);
+        assertEquals(defender.getHealthPoints(), 12);
 
-        assertEquals(test1.getHealthPoints(), 20);
+        assertEquals(attacker.getHealthPoints(), 20);
 
-        test2.attack(test1);
-        assertEquals(test1.getHealthPoints(), 0);
-        assertEquals(test2.getHealthPoints(), 12);
+        defender.attack(attacker);
+        assertEquals(attacker.getHealthPoints(), 0);
+        assertEquals(defender.getHealthPoints(), 12);
 
     }
 
     public void testGetAttack() {
-        CavalryUnit test1 = new CavalryUnit("Test1", 20, 10, 5);
-        CavalryUnit test2 = new CavalryUnit("Test2", 15);
-
-        assertEquals(test1.getAttackPoints(), 10);
-        assertEquals(test2.getAttackPoints(), 20);
+        assertEquals(attacker.getAttackPoints(), 10);
+        assertEquals(defender.getAttackPoints(), 20);
     }
 
     public void testGetArmor() {
-        CavalryUnit test1 = new CavalryUnit("Test1", 20, 10, 5);
-        CavalryUnit test2 = new CavalryUnit("Test2", 15);
-
-        assertEquals(test1.getArmorPoints(), 5);
-        assertEquals(test2.getArmorPoints(), 12);
+        assertEquals(attacker.getArmorPoints(), 5);
+        assertEquals(defender.getArmorPoints(), 12);
     }
 
     public void testTakeDamage() {
-        CavalryUnit test1 = new CavalryUnit("Test1", 15);
-
-        test1.setHealthPoints(2);
-        assertEquals(test1.getHealthPoints(), 13);
+        attacker.setHealthPoints(2);
+        assertEquals(attacker.getHealthPoints(), 13);
     }
 
     public void testToString() {
-        CavalryUnit test1 = new CavalryUnit("Test1", 20, 10, 5);
-
-        assertEquals(test1.toString(), "CavalryUnit{name='Test1', health=20, attack=10, armor=5}");
+        assertEquals(attacker.toString(), "CavalryUnit{name='Test1', health=20, attack=10, armor=5}");
     }
 }
