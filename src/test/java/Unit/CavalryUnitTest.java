7b3a875bc12beae32cc8efc212f28837f7af99a0
Arunan <arunan2212@gmail.com>
2022-02-21 14:55:19 +0100

    Remove testisDead, and optimize tests

--- src/test/java/Unit/CavalryUnitTest.java
+++ src/test/java/Unit/CavalryUnitTest.java
@@ -1,35 +1,61 @@
 package Unit;
-import junit.framework.TestCase;
-import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
-public class CavalryUnitTest extends TestCase {
+class CavalryUnitTest {
 
-    CavalryUnit attacker = new CavalryUnit("Test1", 20);
-    CavalryUnit defender = new CavalryUnit("Test2", 15);
+    CavalryUnit attacker = new CavalryUnit("attacker", 20);
+    CavalryUnit defender = new CavalryUnit("defender", 15);
 
     @Test
-    @DisplayName("Attacking deals x damage")
-    public void testAttack() {
+    @DisplayName("Constructing CavalryUnit wrong")
+    void testWrongConstruction() {
+
+    }
+
+    @Test
+    @DisplayName("Attacking once")
+    void testAttack() {
         attacker.attack(defender);
+
         assertEquals(20, attacker.getHealthPoints());
         assertEquals(2, defender.getHealthPoints());
 
-        //player should be dead
-
-        attacker.attack(defender);
-        assertEquals(20, attacker.getHealthPoints());
-        assertEquals(-11, defender.getHealthPoints());
-
     }
 
     @Test
-    @DisplayName("Playes is dead when HP is less than or equal to 0")
-    public void testisDead() {
-        testAttack();
-        assertEquals(defender.isDead(), true);
+    @DisplayName("Checking attack bonus after attacking once")
+    void testAttackBonus() {
+        int baseAttackBonus = CavalryUnit.BASE_ATTACK_BONUS;
+        int attackBonus = CavalryUnit.BASE_ATTACK_BONUS + CavalryUnit.FIRST_ATTACK_BONUS;
+
+        attacker.attack(defender);
+
+        assertEquals(baseAttackBonus, attacker.getAttackBonus());
+        assertEquals(attackBonus, defender.getAttackBonus());
     }
 
+    @Test
+    @DisplayName("Test death of CavalryUnit")
+    void testDeath() {
+        attacker.attack(defender);
+
+        assertEquals(20, attacker.getHealthPoints());
+        assertEquals(2, defender.getHealthPoints());
+
+        attacker.attack(defender);
+
+        //Unit should be dead
+        assertEquals(20, attacker.getHealthPoints());
+        assertEquals(-7, defender.getHealthPoints());
+
+        assertTrue(defender.isDead());
+        assertFalse(attacker.isDead());
+    }
 
 
 }
