9d4ae43d5f444e4a0042fa54e9bb6783f9db16b7
Arunan <arunan2212@gmail.com>
2022-04-17 14:57:22 +0200

    Fix exception to be checked

--- src/main/java/edu/ntnu/arunang/wargames/exception/FileFormatException.java
+++ src/main/java/edu/ntnu/arunang/wargames/exception/FileFormatException.java
@@ -3,10 +3,10 @@ package edu.ntnu.arunang.wargames.exception;
 /**
  * Exception to be thrown when the file is wrongly formatted or is empty.
  * This should be handled as a checked Exception, because a file that is wrongly
- * formatted or empty can be given by the user, and is not related to the program itself.
+ * formatted or empty can be given by the user, and should not break the program.
  */
 
-public class FileFormatException extends Exception {
+public class FileFormatException extends RuntimeException {
 
     /**
      * Constructs the Exception with a message
