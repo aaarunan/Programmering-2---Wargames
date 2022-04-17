a7d50ed8d0278c175ffb3eead206124b3fbbd376
Arunan <Arunan2212@gmail.com>
2022-04-17 17:21:01 +0200

    Fix maven not compiling

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,6 @@ open module edu.ntnu.arunang.wargames {
     requires javafx.base;
     requires java.desktop;
     exports edu.ntnu.arunang.wargames;
-    exports edu.ntnu.arunang.wargames.unit;
+    exports edu.ntnu.arunang.wargames.gui;
+    exports edu.ntnu.arunang.wargames.gui.controller;
 }
